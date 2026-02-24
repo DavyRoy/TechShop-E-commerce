b1d27627223deaaf2adc70005704aca01cf79568
Sergey <printerprof@yandex.ru>
2026-02-24 15:15:13 +0300

    feat: add Prometheus monitoring for Flask backend

--- devops-projects/techshop-v1/backend/app/__init__.py
+++ devops-projects/techshop-v1/backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from app.config import Config
+from prometheus_flask_exporter import PrometheusMetrics
 
 db = SQLAlchemy()
 
@@ -14,5 +15,6 @@ def create_app():
         db.create_all()
     from app.routes import api
     app.register_blueprint(api)
+    metrics = PrometheusMetrics(app)
 
     return app
