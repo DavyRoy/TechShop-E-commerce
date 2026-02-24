14a8cc9956fb0e7c6bbd71d7be7dc85c64f55064
DavyRoy <125256077+DavyRoy@users.noreply.github.com>
2026-02-24 22:57:51 +0300

    Update __init__.py

--- devops-projects/techshop-v1/backend/app/__init__.py
+++ devops-projects/techshop-v1/backend/app/__init__.py
@@ -5,7 +5,7 @@ from app.config import Config
 from prometheus_flask_exporter import PrometheusMetrics
 
 db = SQLAlchemy()
-
+metrics = PrometheusMetrics()
 def create_app():
     app = Flask(__name__)
     CORS(app)
@@ -15,6 +15,5 @@ def create_app():
         db.create_all()
     from app.routes import api
     app.register_blueprint(api)
-    metrics = PrometheusMetrics(app)
 
     return app
