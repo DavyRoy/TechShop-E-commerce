062cec0f26499037a0c45319ac122e562fe95018
Sergey <printerprof@yandex.ru>
2026-03-06 11:52:21 +0300

    feat: add docker compose for local development

--- devops-projects/techshop-v1/services/order-service/app/__init__.py
+++ devops-projects/techshop-v1/services/order-service/app/__init__.py
@@ -6,12 +6,16 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL',
+        'sqlite:///orders.db'
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     from .routes import orders_bp
-    app.register_blueprint(orders_bp)   
+    app.register_blueprint(orders_bp)
 
     return app
--- devops-projects/techshop-v1/services/user-service/app/__init__.py
+++ devops-projects/techshop-v1/services/user-service/app/__init__.py
@@ -6,7 +6,11 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL',
+        'sqlite:///app.db'
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
